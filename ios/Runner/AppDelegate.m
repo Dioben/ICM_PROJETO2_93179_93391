fa14e941c5efc340547a9dc04c0da8b947e4a65d
buckaroo69 <diogobento@ua.pt>
2020-12-18 15:17:25 +0000

    maps keys

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GMSServices provideAPIKey:@"AIzaSyAvjy91-ciM3s8La6TuiF4zA4LSMZVrv4g"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
