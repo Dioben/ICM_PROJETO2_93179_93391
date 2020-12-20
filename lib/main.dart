7b59de7e7df24d1ce0bae7cc0aa0a0df392aa3ae
buckaroo69 <diogobento@ua.pt>
2020-12-20 23:04:17 +0000

    put style info in the right widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:track_keeper/widgets/start-load.dart';
 
 void main() {
-  runApp(MaterialApp(home: StartLoad(),));
+  runApp(MaterialApp(
+    home: StartLoad(),
+  theme: ThemeData(
+    accentColor: Colors.greenAccent,
+    primaryColor: Colors.green,
+    // This is the theme of your application.
+    //
+    // Try running your application with "flutter run". You'll see the
+    // application has a blue toolbar. Then, without quitting the app, try
+    // changing the primarySwatch below to Colors.green and then invoke
+    // "hot reload" (press "r" in the console where you ran "flutter run",
+    // or simply save your changes to "hot reload" in a Flutter IDE).
+    // Notice that the counter didn't reset back to zero; the application
+    // is not restarted.
+    primarySwatch: Colors.green,
+    elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(primary: Colors.green)),
+  )
+  ));
 }
 
 class MyApp extends StatelessWidget {
