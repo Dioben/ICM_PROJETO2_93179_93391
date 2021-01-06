763f3757aaaded5edb5b58a827f985ecd60c3036
buckaroo69 <diogobento@ua.pt>
2021-01-06 11:55:50 +0000

    map rendering in backend, ui doesnt allow it to display

--- lib/widgets/track-info.dart
+++ lib/widgets/track-info.dart
@@ -1,27 +1,66 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:track_keeper/Queries/FirebaseApiClient.dart';
 import 'package:track_keeper/datamodel/course.dart';
 import 'package:track_keeper/widgets/tracking.dart';
 
-class TrackInfoActivity extends StatelessWidget {
+class TrackInfoActivity extends StatefulWidget {
   Course course;
-  List<Course> courses;
-  StreamSubscription<Course> updateStream;
   @override
 
-  TrackInfoActivity(Course course){this.course=course;updateStream=FirebaseApiClient.instance.getOtherRuns(course).listen((event) {courses.add(event); });}
+  TrackInfoActivity(Course course){this.course=course;}
+
+  @override
+  _TrackInfoActivityState createState() => _TrackInfoActivityState();
+}
+
+class _TrackInfoActivityState extends State<TrackInfoActivity> {
+  List<Course> courses;
+  StreamSubscription<Course> updateStream;
+  Set<Marker> _markers;
+  Set<Polyline> _polylines;
+  CameraPosition initialCameraPosition;
+  List<LatLng> points;
+  GoogleMapController mapController;
+  @override
+  void initState(){
+    super.initState();
+    courses = [];
+    _markers=Set();
+    _polylines=Set();
+    points = [];
+    initialCameraPosition = CameraPosition(
+        target: widget.course.nodes.first.toLatLng(),
+        zoom: CAMERA_ZOOM,
+        tilt: CAMERA_TILT,
+        bearing: CAMERA_BEARING);
+
+    updateStream=FirebaseApiClient.instance.getOtherRuns(widget.course).listen((event) {courses.add(event);setState(() {
+
+  }); });}
 
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(appBar: AppBar(title: Text(course.name),actions: [FlatButton(onPressed:()=> goToFollowing(context), child: const Text("Run"))],),
+    return Scaffold(appBar: AppBar(title: Text(widget.course.name),actions: [FlatButton(onPressed:()=> goToFollowing(context), child: const Text("Run"))],),
     body:  Container(
         padding: EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-
+          /*GoogleMap(
+          myLocationButtonEnabled: true,
+          compassEnabled: true,
+          tiltGesturesEnabled: false,
+          markers: _markers,
+          polylines: _polylines,
+          mapType: MapType.normal,
+          initialCameraPosition: initialCameraPosition,
+          onMapCreated: onMapCreated,
+          zoomControlsEnabled: false,
+        )
+            ,*/
             Column(
               children: [
                 Container(
@@ -32,7 +71,7 @@ class TrackInfoActivity extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("Uploaded by:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text((course==null)?"":course.user+" "+course.getFormattedTimestamp(), style: TextStyle(fontSize: 17)),
+                        Text((widget.course==null)?"":widget.course.user+" "+widget.course.getFormattedTimestamp(), style: TextStyle(fontSize: 17)),
                       ],
                     )
                 ),
@@ -59,7 +98,7 @@ class TrackInfoActivity extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("Length:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text((course==null)?"":course.formattedTrackLength(), style: TextStyle(fontSize: 17)),
+                        Text((widget.course==null)?"":widget.course.formattedTrackLength(), style: TextStyle(fontSize: 17)),
                       ],
                     )
                 ),
@@ -86,7 +125,7 @@ class TrackInfoActivity extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("Runtime:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text((course==null)?"":course.formattedRuntime(), style: TextStyle(fontSize: 17)),
+                        Text((widget.course==null)?"":widget.course.formattedRuntime(), style: TextStyle(fontSize: 17)),
                       ],
                     )
                 ),
@@ -112,7 +151,7 @@ class TrackInfoActivity extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("Maximum speed:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text((course==null)?"":course.formattedMaxSpeed(), style: TextStyle(fontSize: 17)),
+                        Text((widget.course==null)?"":widget.course.formattedMaxSpeed(), style: TextStyle(fontSize: 17)),
                       ],
                     )
                 ),
@@ -138,7 +177,7 @@ class TrackInfoActivity extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("Average speed:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text((course==null)?"":course.formattedAvgSpeed(), style: TextStyle(fontSize: 17)),
+                        Text((widget.course==null)?"":widget.course.formattedAvgSpeed(), style: TextStyle(fontSize: 17)),
                       ],
                     )
                 ),
@@ -164,7 +203,7 @@ class TrackInfoActivity extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text("pictures:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text((course==null)?"":course.pictures.toString(), style: TextStyle(fontSize: 17)),
+                        Text((widget.course==null)?"":widget.course.pictures.toString(), style: TextStyle(fontSize: 17)),
                       ],
                     )
                 ),
@@ -188,5 +227,28 @@ class TrackInfoActivity extends StatelessWidget {
 
   void goToFollowing(context) {
     Navigator.push(context,MaterialPageRoute(builder: (context) => TrackingActivity()));
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    _markers.add(Marker(
+        markerId: MarkerId('Start'),
+        infoWindow: InfoWindow(title: "Start"),
+        position: widget.course.nodes.first.toLatLng()));
+    _markers.add(Marker(
+        markerId: MarkerId('Finish'),
+        infoWindow: InfoWindow(title: "Finish"),
+        position: widget.course.nodes.last.toLatLng()));
+    setState(() {
+
+    });
+    widget.course.unwindCourse().listen((event) {points.add(event);  _polylines.add(Polyline(
+        polylineId: PolylineId('our track'),
+        visible: true,
+        points: points,
+        color: Colors.red));
+    setState(() {});
+    });
+
   }
 }
