a145225c31b05d2b47cf45426f26d5936bd83f3c
p-laranjinha <plcasimiro@ua.pt>
2021-01-10 18:21:29 +0000

    map working in track info

--- lib/widgets/track-info.dart
+++ lib/widgets/track-info.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -61,56 +63,67 @@ class _TrackInfoActivityState extends State<TrackInfoActivity> {
               onPressed: () => goToFollowing(context), child: const Text("Run"))
         ],
       ),
-      body: Container(
-          padding: EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              /*GoogleMap(
-          myLocationButtonEnabled: true,
-          compassEnabled: true,
-          tiltGesturesEnabled: false,
-          markers: _markers,
-          polylines: _polylines,
-          mapType: MapType.normal,
-          initialCameraPosition: initialCameraPosition,
-          onMapCreated: onMapCreated,
-          zoomControlsEnabled: false,
-        )
-            ,*/
-              TrackItemField(title: "Track name:", value: widget.course.name),
-              TrackItemField(title: "Runner name:", value: widget.course.user),
-              TrackItemField(
-                  title: "Date uploaded:",
-                  value: widget.course.getFormattedTimestamp()),
-              TrackItemField(
-                  title: "Length:",
-                  value: widget.course.formattedTrackLength()),
-              TrackItemField(
-                  title: "Runtime:", value: widget.course.formattedRuntime()),
-              TrackItemField(
-                  title: "Rating:", value: widget.course.rating.toString()),
-              TrackItemField(
-                  title: "Distance away:",
-                  value: (() {
-                    if (currLon != null && currLat != null)
-                      return widget.course.formattedDistance(currLat, currLon);
-                    else
-                      return "Unknown";
-                  })()),
-              TrackItemField(
-                  title: "Maximum speed:",
-                  value: widget.course.formattedMaxSpeed()),
-              TrackItemField(
-                  title: "Average speed:",
-                  value: widget.course.formattedAvgSpeed()),
-              TrackItemField(
-                  title: "pictures:",
-                  value: (widget.course == null)
-                      ? ""
-                      : widget.course.pictures.toString()),
-            ],
-          )),
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height/2,
+            width: double.infinity,
+            margin: EdgeInsets.fromLTRB(20, 10 , 20, 10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).primaryColor
+              )
+            ),
+            child: GoogleMap(
+              myLocationButtonEnabled: true,
+              compassEnabled: true,
+              tiltGesturesEnabled: false,
+              markers: _markers,
+              polylines: _polylines,
+              mapType: MapType.normal,
+              initialCameraPosition: initialCameraPosition,
+              onMapCreated: onMapCreated,
+              zoomControlsEnabled: false,
+              gestureRecognizers: {
+                Factory<OneSequenceGestureRecognizer>(
+                  () => EagerGestureRecognizer(),
+                ),
+              },
+            ),
+          ),
+          TrackItemField(title: "Track name:", value: widget.course.name),
+          TrackItemField(title: "Runner name:", value: widget.course.user),
+          TrackItemField(
+              title: "Date uploaded:",
+              value: widget.course.getFormattedTimestamp()),
+          TrackItemField(
+              title: "Length:",
+              value: widget.course.formattedTrackLength()),
+          TrackItemField(
+              title: "Runtime:", value: widget.course.formattedRuntime()),
+          TrackItemField(
+              title: "Rating:", value: widget.course.rating.toString()),
+          TrackItemField(
+              title: "Distance away:",
+              value: (() {
+                if (currLon != null && currLat != null)
+                  return widget.course.formattedDistance(currLat, currLon);
+                else
+                  return "Unknown";
+              })()),
+          TrackItemField(
+              title: "Maximum speed:",
+              value: widget.course.formattedMaxSpeed()),
+          TrackItemField(
+              title: "Average speed:",
+              value: widget.course.formattedAvgSpeed()),
+          TrackItemField(
+              title: "pictures:",
+              value: (widget.course == null)
+                  ? ""
+                  : widget.course.pictures.toString()),
+        ],
+      ),
     );
   }
 
