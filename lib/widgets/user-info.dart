5502e7af1d17665d3f773e39af8f2ab969373884
p-laranjinha <plcasimiro@ua.pt>
2021-01-10 23:10:09 +0000

    fixed user info

--- lib/widgets/user-info.dart
+++ lib/widgets/user-info.dart
@@ -14,198 +14,65 @@ class UserStats extends StatelessWidget{
     return Scaffold(appBar: AppBar(title: Text("User Stats"),),
         body:
         Container(
-            padding: EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
+            padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 80, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Name:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.username, style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 80, 10),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Top Score:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.top_rating.toString(), style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 20, 10),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Track Count:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.coursecount.toString(), style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 20, 10),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Total Track Length:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.formattedTrackLength(), style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 20, 10),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Total Runtime:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.formattedRuntime(), style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 20, 6),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Max Speed:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.formattedMaxSpeed(), style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 20, 6),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                Column(
-                  children: [
-                    Container(
-                        height: 20,
-                        width: double.infinity,
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Average Speed:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            Text(user.formattedAvgSpeed(), style: TextStyle(fontSize: 17)),
-                          ],
-                        )
-                    ),
-                    Container(
-                      height: 3,
-                      width: double.infinity,
-                      margin: EdgeInsets.fromLTRB(20, 3, 20, 6),
-                      child: SizedBox(
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.green),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                UserItemField(title: "Name:", value: user.username,),
+                UserItemField(title: "Top Score:", value: user.top_rating.toString(),),
+                UserItemField(title: "Track Count:", value: user.coursecount.toString(),),
+                UserItemField(title: "Total Track Length:", value: user.formattedTrackLength(),),
+                UserItemField(title: "Total Runtime:", value: user.formattedRuntime(),),
+                UserItemField(title: "Max Speed:", value: user.formattedMaxSpeed(),),
+                UserItemField(title: "Average Speed:", value: user.formattedAvgSpeed(),),
               ],
             )
 
         )
 
+    );
+  }
+}
+
+class UserItemField extends StatefulWidget {
+  UserItemField({Key key, @required this.title, @required this.value})
+      : super(key: key);
+  final String title;
+  final String value;
+
+  @override
+  _UserItemFieldState createState() => new _UserItemFieldState();
+}
+
+class _UserItemFieldState extends State<UserItemField> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            height: 20,
+            width: double.infinity,
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.title,
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Text(widget.value, style: TextStyle(fontSize: 17)),
+              ],
+            )),
+        Container(
+          height: 2,
+          width: double.infinity,
+          margin: EdgeInsets.fromLTRB(20, 3, 20, 10),
+          child: SizedBox(
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: Colors.green),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
